46e776b9a4ad1dce907af63ca55151d30c39c680
vit.samchuk <vit.samchuk@gmail.com>
2025-06-26 13:58:29 +0300

    fix configs.route index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require("./auth.route");
-const configs = require('/configs.route')
+const configs = require('./configs.route')
 
 router.get('/', (req, res) => res.send("<h1>Welcome to Nginx Dashboard v0.0.1!</h1>"))
 router.use('/auth', auth);
